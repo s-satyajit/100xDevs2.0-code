da7d524c13b51c564756d67f5664656ed2f3deb6
Satyajit Samal <samalsatyajit668@gmail.com>
2024-12-27 19:25:31 +0530

    Add initial implementation of WEEK_01 with basic JavaScript examples

---
+++ WEEK_01/main.js
@@ -0,0 +1,133 @@
+let a = 1
+a = 2;
+console.log(a)
+
+let firstnanme = "Satyajit"
+let age = 23;
+let isMarried =  false;
+
+console.log(`this person's name is ${firstnanme} and their age is ${age}`)
+
+if (isMarried) 
+    console.log(`${firstnanme} is married`)
+else
+    console.log(`${firstnanme} is not married`)
+
+
+// for(let answer = 0; answer <= 100; answer++) {
+//     console.log(answer + "")
+// }
+
+// let answer = 0
+// for(let i = 0; i <= 10; i++) {
+//     answer = answer + i;
+//     console.log(answer)
+// }
+// console.log(answer)
+
+const ages = [21, 22, 23, 24, 25];
+
+// for(let i = 0; i < ages.length; i++)  {
+//     if(ages[i] % 2 == 0)
+//         console.log(ages[i])
+// }
+
+const personArray = ["harkirat", "raman", "priya"]
+const genderArray = ["male", "male", "female"]
+
+// for(let i = 0; i < personArray.length; i++) {
+//     if(genderArray[i] == "male")
+//         console.log(personArray[i])
+// }
+
+// const user1 = {
+//     firstName: "Harkirat",
+//     gender: "Male"
+// }
+
+// console.log(user1["firstName"])
+
+const allUsers = [{
+    firstName: "Harkirat",
+    gender: "Male", 
+    metadata: {
+        age: 21,
+        address: ""
+    }
+}, {
+    firstName: "Raman",
+    gender: "Male",
+    metadata: { 
+        age: 22,
+        address: ""
+    }
+}, {
+    firstName: "Priya",
+    gender: "Female",
+    metadata: {
+        age: 23,
+        address: ""
+    }
+}]
+
+// for(let i = 0; i < allUsers.length; i++) {
+//     if(allUsers[i]["metadata"]["age"] >= 22)
+//         console.log(allUsers[i]["firstName"])
+// }
+
+// function sum(a, b) {
+//     const sumValue = a + b
+//     return sumValue
+// }
+
+// const value = sum(1, 2)
+// console.log(value)
+
+// function sum(a, b)  {
+//     return a + b
+// }
+
+// function displayResult(data) {
+//     console.log(`Result of the sum is: ${data}`)
+// }
+// function displayResultPassive(data) {
+//     console.log(`Sum's result is: ${data}`)
+// }
+
+// const ans = sum(1, 2)
+// console.log(displayResult(ans))
+// console.log(displayResultPassive(ans))
+
+// const sum = (num1, num2, fnToCall) => {
+//     let result = num1 + num2;
+//     fnToCall(result)
+// }
+
+// const displayResult = (data) => {
+//     console.log(`Result of the sum is: ${data}`)
+// }
+
+// const displayResultPassive = (data) => {
+//     console.log(`Sum's result is: ${data}`)
+// }
+
+// let ans = sum(1, 2, displayResult)
+// console.log(ans)
+
+// const calculateArithmetic = (a, b, type) => {
+//     if(type == "sum")
+//         return sum(a, b)
+//     if(type == "sub")
+//         return sub(a, b)
+// }
+
+// const sum = (a, b) => {
+//     return a + b
+// }
+
+// const sub = (a, b) => {
+//     return a - b
+// }
+
+// console.log(calculateArithmetic(5, 2, "sub"))
+
