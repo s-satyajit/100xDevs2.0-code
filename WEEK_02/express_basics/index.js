0ebb357dee2d0267b93bf68205879a48a49cc0c0
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-11 13:05:40 +0530

    created WEEK_02, and added index.js

---
+++ WEEK_02/express_basics/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const calculateSum = (n) => {
+  let ans = 0;
+  for (let i = 0; i <= n; i++) {
+    ans = ans + i;
+  }
+  return ans;
+};
+
+app.get('/', (req, res) => {
+    const n = req.query.n
+    const ans = calculateSum(n)
+    res.send(`Your ans is: ${ans}`)
+})
+
+app.listen(3000)
