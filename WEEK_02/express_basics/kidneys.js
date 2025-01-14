e90f74cc9a11b84f4d925b86fda5fb5d65e17946
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-14 09:26:18 +0530

    Add kidneys.js with GET and POST endpoints for kidney health tracking

---
+++ WEEK_02/express_basics/kidneys.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+app.listen(3000)
+
+const users = [{
+    name: "John",
+    kidneys: [{
+        healthy: false
+    }]
+}]
+
+app.get('/', (req, res) => {
+    const johnKidneys = users[0].kidneys
+    const numberOfKidneys = johnKidneys.length
+    let numberOfHealthyKidneys = 0;
+    for(let i = 0; i < numberOfKidneys; i++) {
+        if(johnKidneys.healthy)
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys,
+        johnKidneys,
+    })
+})
+
+app.post('/', (req, res) => {
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "Done!"
+    })
+})
