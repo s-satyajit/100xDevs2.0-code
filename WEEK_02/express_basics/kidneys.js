4ca9293d5ff4a7223f5306cbfb5459fc86c0723a
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-19 21:48:43 +0530

    wip

--- WEEK_02/express_basics/kidneys.js
+++ WEEK_02/express_basics/kidneys.js
@@ -1,38 +1,42 @@
-const express = require("express")
-const app = express()
-app.listen(3000)
+const express = require("express");
+const app = express();
+app.listen(3000);
 
-const users = [{
+const users = [
+  {
     name: "John",
-    kidneys: [{
-        healthy: false
-    }]
-}]
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
 
-app.get('/', (req, res) => {
-    const johnKidneys = users[0].kidneys
-    const numberOfKidneys = johnKidneys.length
-    let numberOfHealthyKidneys = 0;
-    for(let i = 0; i < numberOfKidneys; i++) {
-        if(johnKidneys.healthy)
-            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
-    }
-    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+app.get("/", (req, res) => {
+  const johnKidneys = users[0].kidneys;
+  const numberOfKidneys = johnKidneys.length;
+  let numberOfHealthyKidneys = 0;
+  for (let i = 0; i < numberOfKidneys; i++) {
+    if (johnKidneys.healthy)
+      numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+  }
+  const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
 
-    res.json({
-        numberOfKidneys,
-        numberOfHealthyKidneys,
-        numberOfUnhealthyKidneys,
-        johnKidneys,
-    })
-})
+  res.json({
+    numberOfKidneys,
+    numberOfHealthyKidneys,
+    numberOfUnhealthyKidneys,
+    johnKidneys,
+  });
+});
 
-app.post('/', (req, res) => {
-    const isHealthy = req.body.isHealthy;
-    users[0].kidneys.push({
-        healthy: isHealthy
-    })
-    res.json({
-        msg: "Done!"
-    })
-})
+app.post("/", (req, res) => {
+  const isHealthy = req.body.isHealthy;
+  users[0].kidneys.push({
+    healthy: isHealthy,
+  });
+  res.json({
+    msg: "Done!",
+  });
+});
