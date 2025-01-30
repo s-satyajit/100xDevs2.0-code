0e98cca929805499961b3f74a4372013502cb6a9
Satyajit Samal <samalsatyajit668@gmail.com>
2025-01-30 09:36:56 +0530

    Added middlewares

---
+++ WEEK_03/middleware/index.js
@@ -0,0 +1,68 @@
+// User needs to send a kidneyId as a query param which should be a number from 1-2 (humans only has 2 kidneys)
+// User should send a username and password in headers
+
+import express from "express"
+const app = express()
+
+// app.get("/health-checkup", (req, res) => {
+    
+//     const kidneyId = req.query.kidneyId;
+//     const username = req.headers.username;
+//     const password = req.headers.password;
+
+//     if(username != "satyajit" || password != "pass") {
+//         res.status(403).json({
+//             "msg": "User not found!"
+//         })
+//         return
+//     }
+
+//     if(kidneyId != 1 && kidneyId != 2) {
+//         res.status(411).json({
+//             "msg": "Wrong inputs!"
+//         })
+//         return
+//     }
+
+//     res.send("Your heart is healthy!")
+
+// })
+
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on ${PORT}`)
+// })
+
+// ------------------------------------------  we can also write
+
+const userValidator = (username, password) => {
+    if(username != "satyajit" && password != "pass") {
+        return false;
+    }
+    return true;
+}
+
+const kidneyValidator = (kidney) => {
+    if(kidney != 1 && kidney != 2) {
+        return false
+    }
+    return true
+}
+
+app.get("/health-checkup", (req, res) => {
+    if(userValidator(req.header.username)) {
+        res.status(403).json({
+            "msg": "User doesn't exist"
+        })
+        return
+    }
+
+    if(kidneyValidator(req.query.kidneyId)) {
+        res.status(411).json({
+            "msg": "Wrong input"
+        })
+        return
+    }
+
+    res.send("Your kidney is healthy!")
+})
