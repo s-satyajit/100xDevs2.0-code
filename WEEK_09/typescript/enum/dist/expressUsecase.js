5a4cd3ebb99fdfaba321aca8d77adc8ed1e923af
Satyajit Samal <samalsatyajit668@gmail.com>
2026-01-14 11:33:18 +0530

    modified express usecase of enum

--- WEEK_09/typescript/enum/dist/expressUsecase.js
+++ WEEK_09/typescript/enum/dist/expressUsecase.js
@@ -1,22 +1,22 @@
 import express, {} from 'express';
-const app = express();
-const PORT = 8080;
 var ResponseStatus;
 (function (ResponseStatus) {
-    ResponseStatus[ResponseStatus["Error"] = 500] = "Error";
-    ResponseStatus[ResponseStatus["NotFound"] = 404] = "NotFound";
     ResponseStatus[ResponseStatus["Success"] = 200] = "Success";
+    ResponseStatus[ResponseStatus["NotFound"] = 404] = "NotFound";
+    ResponseStatus[ResponseStatus["Error"] = 500] = "Error";
 })(ResponseStatus || (ResponseStatus = {}));
-app.get('/', (req, res) => {
+const app = express();
+const PORT = 8080;
+app.get("/", (req, res) => {
     res.status(ResponseStatus.Success).json({ msg: "System Online" });
 });
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
     const userId = req.query.userId;
     if (!userId)
-        res.status(ResponseStatus.Error).json({ error: "userid not found" });
-    res.status(ResponseStatus.Success).json({ status: "active" });
+        res.status(ResponseStatus.NotFound).json({ msg: "UserId not found" });
+    res.status(ResponseStatus.Success).json({ server: "active" });
 });
 app.listen(PORT, () => {
-    console.log(`[SERVER]: API running on port: ${PORT}`);
+    console.log(`[SERVER]: API  running on port: ${PORT}`);
 });
 //# sourceMappingURL=expressUsecase.js.map
--- WEEK_09/typescript/enum/src/expressUsecase.ts
+++ WEEK_09/typescript/enum/src/expressUsecase.ts
@@ -1,25 +1,29 @@
-import express, {type Request, type Response, type Application} from 'express';
+import express, {
+  type Request,
+  type Response,
+  type Application,
+} from "express";
+
+enum ResponseStatus {
+  Success = 200,
+  NotFound = 404,
+  Error = 500,
+}
 
 const app: Application = express();
 const PORT: number = 8080;
 
-enum ResponseStatus {
-    Error = 500,
-    NotFound = 404,
-    Success = 200
-}
+app.get("/", (req: Request, res: Response) => {
+  res.status(ResponseStatus.Success).json({ msg: "System Online" });
+});
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(ResponseStatus.Success).json({msg: "System Online"});
-})
-
-app.get('/users', (req: Request, res: Response) => {
-    const userId = req.query.userId;
-    if(!userId)
-        res.status(ResponseStatus.Error).json({error: "userid not found"});
-    res.status(ResponseStatus.Success).json({status: "active"});
-})
+app.get("/users", (req: Request, res: Response) => {
+  const userId = req.query.userId;
+  if (!userId)
+    res.status(ResponseStatus.NotFound).json({ msg: "UserId not found" });
+  res.status(ResponseStatus.Success).json({ server: "active" });
+});
 
 app.listen(PORT, () => {
-    console.log(`[SERVER]: API running on port: ${PORT}`);
-})
+  console.log(`[SERVER]: API  running on port: ${PORT}`);
+});
